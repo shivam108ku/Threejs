301d3a35a4e404b75494e15ddf07ef09b1b1bc08
shivam108ku <shivam108ku@gmail.com>
2025-03-10 16:29:10 +0530

    second

--- src/main.js
+++ src/main.js
@@ -1,11 +1,39 @@
 import * as THREE from "three";
 const scene = new THREE.Scene();
+import gsap from 'gsap'
 
-const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshBasicMaterial({color: 'red'});
-const  mesh = new THREE.Mesh(geometry,material);
+// const geometry = new THREE.BoxGeometry(1,1,1);
+// const material = new THREE.MeshBasicMaterial({color: 'red'});
+// const  mesh = new THREE.Mesh(geometry,material);
 
-scene.add(mesh);
+// mesh.position.set(0.7 , -0.6 , 1);
+// mesh.scale.y = 0.5
+// mesh.rotation.reorder('XYZ');
+// mesh.rotation.set(1,3,5);
+
+ 
+
+// Axis Helper
+// const axishelper = new THREE.AxesHelper(0);
+
+// const group = new THREE.Group()
+// scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1,1,1),
+  new THREE.MeshBasicMaterial({color:'red' })
+)
+
+// const cube2 = new THREE.Mesh(
+//   new THREE.BoxGeometry(1,1,1),
+//   new THREE.MeshBasicMaterial({color:'red'})
+// )
+
+// const cube3 = new THREE.Mesh(
+//   new THREE.BoxGeometry(1,1,1),
+//   new THREE.MeshBasicMaterial({color:'red'})
+// )
+scene.add(cube1);
 
 // Sizes
 
@@ -24,6 +52,32 @@ const renderer = new THREE.WebGLRenderer({
   canvas
 })
 renderer.setSize(sizes.width , sizes.height);
-renderer.render(scene , camera);
+
+let time = Date.now()
+gsap.to(cube1.position, {
+  duration: 1,
+  delay: 1,
+  x:0,
+  
+})
+
+// Animation
+
+const animation = ()=>{
+
+const current = Date.now();
+const deltatime = current - time;
+time = current;
+
+ 
+  console.log("first");
+
+  cube1.rotation.y += 0.01
+  // Objects
+  renderer.render(scene , camera);
+
+  window.requestAnimationFrame(animation);
+}
+animation();
 
   
